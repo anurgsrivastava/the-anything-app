6ab2f7398612de287c97b70f7977014c4f5b9b08
anurgsrivastava <srivastavaanurag20@gmail.com>
2020-04-29 03:06:42 +0530

    splash screen and app icons added

--- app/App.js
+++ app/App.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import SplashScreen from "react-native-splash-screen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello!</Text>
-    </View>
-  );
+export default class App extends React.PureComponent() {
+  componentDidMount() {
+    SplashScreen.hide();
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello Chinni!</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
